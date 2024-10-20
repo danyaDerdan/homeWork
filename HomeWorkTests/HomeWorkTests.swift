91a40e6c1d4599a2b7bcd86bd48317ea33a2256a
danyaDerdan <110595066+danyaDerdan@users.noreply.github.com>
2024-10-20 20:07:56 +0300

    Initial Commit

---
+++ HomeWorkTests/HomeWorkTests.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWorkTests.swift
+//  HomeWorkTests
+//
+//  Created by Данил Толстиков on 20.10.2024.
+//
+
+import Testing
+@testable import HomeWork
+
+struct HomeWorkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
