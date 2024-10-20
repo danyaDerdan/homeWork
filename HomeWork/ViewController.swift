800cf1048fa39de017910348677e868df85a1160
danyaDerdan <110595066+danyaDerdan@users.noreply.github.com>
2024-10-20 20:21:14 +0300

    Made the app label

--- HomeWork/ViewController.swift
+++ HomeWork/ViewController.swift
@@ -4,8 +4,31 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setup()
     }
-
+    
+    
+    //MARK: - Fields
+    private let label : UILabel = {
+        let view = UILabel()
+        view.text = "Sigma App"
+        view.textAlignment = .center
+        view.font = .systemFont(ofSize: .init(40))
+        return view
+    }()
+    
+    //MARK: - Constraints
+    func createLabelConstraints() {
+        label.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label)
+        label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        label.centerYAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+    }
+    
+    //MARK: - Functions
+    func setup() {
+        createLabelConstraints()
+    }
 
 }
 
